684ec61520f20a9e042db68f44707199e9431ccf
Tatevik Avoyan <Tatevik.avoian@gmail.com>
2025-03-07 22:27:15 +0400

    Mark tasks as solved and add Type Error examples.
    Updated the status of several YAML files to "Solved" and modified record values. Added examples to demonstrate string manipulation, data types, and conversion in Python for Day 2 tasks. Minor internal changes noted in storage files.

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,29 @@
-len("Hello")
+name = "Tatiana"
+age = 23
+height = 1.75
+is_new = True
+
+# Subscripting
+print(name[0])
+
+# String
+print("123" + "345")
+
+num1 = 123
+num2 = 345
+# Integer = Whole number
+print(num1 + num2)
+
+# Large Integers
+print(123_456_789)
+
+# Float = Floating Point Number
+print(height)
+
+# Boolean
+print(True)
+print(False)
+
+
+
+
